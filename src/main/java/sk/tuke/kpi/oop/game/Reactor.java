8790160057e0ff639febbd7393a318a024f13bdf
zmudovakatka <katka.zmudova@gmail.com>
2023-06-20 13:15:42 +0200

    increaseTemperature na pokazenie reaktora

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -32,11 +32,32 @@ public class Reactor extends AbstractActor {
 
     public void increaseTemperature(int increment) {
         this.temperature += increment;
-        if (this.temperature>=2000 && this.temperature<=6000) {
+        if (this.temperature>=2000 ) {
+            if (this.temperature>=6000){
+                this.damage=100;
+                return;
+            }
             int damage = this.temperature / 40 - 50;
             if (this.damage<damage){
                 this.damage=damage;
             }
+            if (this.temperature>=6000){
+                setAnimation(new Animation("sprites/reactor_hot.png",
+                    80,
+                    80,
+                    0.05f,
+                    Animation.PlayMode.LOOP_PINGPONG));
+                return;
+            }
+            if (this.temperature>=4000){
+                setAnimation(new Animation("sprites/reactor_hot.png",
+                    80,
+                    80,
+                    0.05f,
+                    Animation.PlayMode.LOOP_PINGPONG));
+                return;
+            }
+
         }
 
 
