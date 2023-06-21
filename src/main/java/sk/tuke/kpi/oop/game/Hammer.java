3963e0dda50ce285ea40b1284327424b3e6a45d2
zmudovakatka <katka.zmudova@gmail.com>
2023-06-21 12:58:09 +0200

    vytvorenie triedy Hammer

---
+++ src/main/java/sk/tuke/kpi/oop/game/Hammer.java
@@ -0,0 +1,20 @@
+package sk.tuke.kpi.oop.game;
+
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+import sk.tuke.kpi.gamelib.graphics.Animation;
+
+public class Hammer extends AbstractActor {
+    private int usages;
+
+    public Hammer(){
+        this.usages = 1;
+        Animation animation = new Animation("sprites/hammer.png");
+        setAnimation(animation);
+
+
+    }
+
+    public int getUsages() {
+        return this.usages;
+    }
+}
