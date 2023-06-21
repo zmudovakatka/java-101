e7d1164d163981bd47ccf0fb86ede458e05804da
zmudovakatka <katka.zmudova@gmail.com>
2023-06-21 09:46:06 +0200

    vytvorenie clenskych premennych pre animacie

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -8,13 +8,25 @@ public class Reactor extends AbstractActor {
     private boolean state;
     private int damage;
     private Animation normalAnimation;
+    private Animation hotAnimation;
+    private Animation brokenAnimation;
 
     public Reactor() {
-        this.temperature = 0;
         this.state = false;
         this.damage = 0;
+        this.temperature = 0;
         this.normalAnimation = new Animation(
             "sprites/reactor_on.png",
+            80,
+            80,
+            0.1F, Animation.PlayMode.LOOP_PINGPONG);
+        this.hotAnimation = new Animation(
+            "sprites/reactor_hot.png",
+            80,
+            80,
+            0.1F, Animation.PlayMode.LOOP_PINGPONG);
+        this.brokenAnimation = new Animation(
+            "sprites/reactor_broken.png",
             80,
             80,
             0.1F, Animation.PlayMode.LOOP_PINGPONG);
@@ -35,23 +47,35 @@ public class Reactor extends AbstractActor {
 
         //update animation
         if (this.temperature >= 6000) {
-            Animation animation = new Animation("sprites/reactor_broken.png",
-                80, 80, 0.1f,
-                Animation.PlayMode.LOOP_PINGPONG);
-            setAnimation(animation);
+//            Animation animation = new Animation("sprites/reactor_broken.png",
+//                80, 80, 0.1f,
+//                Animation.PlayMode.LOOP_PINGPONG);
+//            setAnimation(animation);
+            setAnimation(this.brokenAnimation);
         } else if (this.temperature >= 4000) {
-            Animation animation = new Animation("sprites/reactor_hot.png",
-                80, 80, 0.1f,
-                Animation.PlayMode.LOOP_PINGPONG);
-            setAnimation(animation);
+//            Animation animation = new Animation("sprites/reactor_hot.png",
+//                80, 80, 0.1f,
+//                Animation.PlayMode.LOOP_PINGPONG);
+//            setAnimation(animation);
+            setAnimation(this.hotAnimation);
 
         } else  {
-            Animation animation = new Animation("sprites/reactor_on.png",
-                80, 80, 0.1f,
-                Animation.PlayMode.LOOP_PINGPONG);
-            setAnimation(animation);
+//            nastavenie normalnej animacie ale vieme vyuzit normalAnimation z konstruktora Reactor
+
+//            Animation animation = new Animation("sprites/reactor_on.png",
+//                80, 80, 0.1f,
+//                Animation.PlayMode.LOOP_PINGPONG);
+//            setAnimation(animation);
+            setAnimation(this.normalAnimation);
 
         }
+
+        if(this.temperature>2000){
+
+
+        }
+
+        int damage = this.temperature/40 - 50;
 
 
     }
