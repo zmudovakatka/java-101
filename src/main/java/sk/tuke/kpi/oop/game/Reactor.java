1cede3b9bd2aff33a9bfb921c7c9ab9d0d9be079
zmudovakatka <katka.zmudova@gmail.com>
2023-06-21 09:06:58 +0200

    podmienky pre animaciu reaktora

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -32,34 +32,26 @@ public class Reactor extends AbstractActor {
 
     public void increaseTemperature(int increment) {
         this.temperature += increment;
-        if (this.temperature>=2000 ) {
-            if (this.temperature>=6000){
-                this.damage=100;
-                return;
-            }
-            int damage = this.temperature / 40 - 50;
-            if (this.damage<damage){
-                this.damage=damage;
-            }
-            if (this.temperature>=6000){
-                setAnimation(new Animation("sprites/reactor_hot.png",
-                    80,
-                    80,
-                    0.05f,
-                    Animation.PlayMode.LOOP_PINGPONG));
-                return;
-            }
-            if (this.temperature>=4000){
-                setAnimation(new Animation("sprites/reactor_hot.png",
-                    80,
-                    80,
-                    0.05f,
-                    Animation.PlayMode.LOOP_PINGPONG));
-                return;
-            }
+
+        //update animation
+        if (this.temperature >= 6000) {
+            Animation animation = new Animation("sprites/reactor_broken.png",
+                80, 80, 0.1f,
+                Animation.PlayMode.LOOP_PINGPONG);
+            setAnimation(animation);
+        } else if (this.temperature >= 4000) {
+            Animation animation = new Animation("sprites/reactor_hot.png",
+                80, 80, 0.1f,
+                Animation.PlayMode.LOOP_PINGPONG);
+            setAnimation(animation);
+
+        } else  {
+            Animation animation = new Animation("sprites/reactor_on.png",
+                80, 80, 0.1f,
+                Animation.PlayMode.LOOP_PINGPONG);
+            setAnimation(animation);
 
         }
-
 
 
     }
